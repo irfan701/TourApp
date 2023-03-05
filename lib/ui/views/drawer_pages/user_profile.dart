3c1eaf745094b6d567e3f0bfe0997eab82b15f7a
irfan701 <irfanhossain701@gmail.com>
2023-03-06 02:10:32 +0600

    Data Select in Profile Form

--- lib/ui/views/drawer_pages/user_profile.dart
+++ lib/ui/views/drawer_pages/user_profile.dart
@@ -1,10 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:toggle_switch/toggle_switch.dart';
-import 'package:tour_app/ui/route/route.dart';
-
-import '../../../const/app_color.dart';
 import '../../styles/style.dart';
 import '../../widgets/violet_btn.dart';
 
@@ -17,6 +16,7 @@ class UserProfileScreen extends StatelessWidget {
   Rx<TextEditingController> _dobController = TextEditingController().obs;
 
   Rx<DateTime> selectedDate = DateTime.now().obs;
+  var gender = 'Male';
 
   selecDate(BuildContext context) async {
     final selected = await showDatePicker(
@@ -31,6 +31,60 @@ class UserProfileScreen extends StatelessWidget {
     }
   }
 
+  setUserData(data, context) {
+    _nameController.text = data['name'];
+    _phoneController.text = data['phone'].toString();
+    _addressController.text = data['address'];
+    _dobController.value.text = data['dob'];
+    gender = data['gender'];
+
+    return Padding(
+      padding: EdgeInsets.only(left: 30.w, right: 30.w, top: 40.h),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        SizedBox(
+          height: 10.h,
+        ),
+        FormField(_nameController, TextInputType.name, 'Name'),
+        FormField(_phoneController, TextInputType.phone, 'Number'),
+        FormField(_addressController, TextInputType.text, 'Address'),
+        Obx(
+          () => TextFormField(
+            controller: _dobController.value,
+            readOnly: true,
+            decoration: InputDecoration(
+                hintText: "Date of birth",
+                hintStyle:
+                    TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w400),
+                suffixIcon: IconButton(
+                  onPressed: () => selecDate(context),
+                  icon: Icon(Icons.calendar_month),
+                )),
+          ),
+        ),
+        SizedBox(
+          height: 10.h,
+        ),
+        ToggleSwitch(
+          initialLabelIndex: gender == 'Male' ? 0 : 1,
+          totalSwitches: 2,
+          labels: ['Male', 'Female'],
+          onToggle: (index) {
+            if (index == 0) {
+              gender = "Male";
+            } else {
+              gender = "Female";
+            }
+            print('switched to: $index');
+          },
+        ),
+        SizedBox(
+          height: 30.h,
+        ),
+        VioletBtn('Update', () {}),
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,49 +93,22 @@ class UserProfileScreen extends StatelessWidget {
           elevation: 0,
           title: Text("Profile"),
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(left: 30.w, right: 30.w, top: 40.h),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(
-                height: 10.h,
-              ),
-              FormField(_nameController, TextInputType.emailAddress, 'Name'),
-              FormField(_phoneController, TextInputType.emailAddress, 'Number'),
-              FormField(
-                  _addressController, TextInputType.emailAddress, 'Address'),
-              Obx(
-                () => TextFormField(
-                  controller: _dobController.value,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                      hintText: "Date of birth",
-                      hintStyle: TextStyle(
-                          fontSize: 16.sp, fontWeight: FontWeight.w400),
-                      suffixIcon: IconButton(
-                        onPressed: () => selecDate(context),
-                        icon: Icon(Icons.calendar_month),
-                      )),
-                ),
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              ToggleSwitch(
-                initialLabelIndex: 0,
-                totalSwitches: 2,
-                labels: ['Male', 'Female'],
-                onToggle: (index) {
-                  print('switched to: $index');
-                },
-              ),
-              SizedBox(
-                height: 30.h,
-              ),
-              VioletBtn('Update', () {}),
-            ]),
-          ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users-form-data')
+              .doc(FirebaseAuth.instance.currentUser!.email)
+              .snapshots(),
+          builder: (context, snapshot) {
+            var data = snapshot.data;
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return setUserData(data, context);
+            }
+          },
         ));
   }
 }
